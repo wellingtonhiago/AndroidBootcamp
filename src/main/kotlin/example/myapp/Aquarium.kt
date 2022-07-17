0ca1806d571f5e523590f7b193bae540eb2d5b6e
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-07-17 17:48:47 -0300

    Mudanças na classe

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -6,21 +6,21 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
     constructor(numberOfFish: Int) : this(){
         val tank = numberOfFish * 2000 * 1.1
         height = (tank / (length * width)).toInt()
-        println("Volume: ${width * length * height / 1000} l")
     }
+
+    val volume: Int
+        get() = width * height * length / 1000
 
     // Iniciadores
     init {
         println("inicializando Aquarium")
     }
 
-    init {
-        println("Volume: ${width * length * height / 1000} l")
-    }
 
 
     fun printSize() {
         println("Width: $width cm Length: $length cm Height: $height cm")
+        println("Volume: $volume l")
     }
 
 }
