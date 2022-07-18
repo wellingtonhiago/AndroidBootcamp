7af024d3e53bdfb5c7319774ee00ed56398b6e69
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-07-17 23:04:50 -0300

    classes de dados

--- src/main/kotlin/example/myapp/decor/Decoration.kt
+++ src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -1,4 +1,27 @@
 package example.myapp.decor
 
-class Decoration {
+data class Decoration(val rocks: String) {
+
+}
+
+fun makeDecorations() {
+    val decoration1 = Decoration("granite")
+    println(decoration1)
+
+    val decoration2 = Decoration("marmore")
+    println(decoration2)
+
+    val decoration3 = Decoration("slate")
+    println(decoration3)
+
+    val decoration4 = Decoration("slate")
+    println(decoration4)
+
+    println(decoration1.equals(decoration2))
+    println(decoration3.equals(decoration4))
+
+}
+
+fun main() {
+    makeDecorations()
 }
